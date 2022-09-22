af3a1b96a9703db8ff3a646a854355b7a0cf4ca6
HK-SHAO <hk-shao@foxmail.com>
2022-09-22 15:22:57 +0800

    init

---
+++ assets/scripts/framework/Constant.ts
@@ -0,0 +1,10 @@
+export class Constant {
+
+    // CONFIG
+    public static GAME_NAME = 'SoCoding-1024-Puzzle-Game-2022';
+    public static GAME_VERSION = '1.0.0';
+    public static GAME_DESCRIPTION = 'SoCoding的1024节解谜游戏';
+    public static GAME_AUTHOR = 'HK-SHAO';
+
+}
+
