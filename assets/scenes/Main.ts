268e882a0dfdc8df42bca6ac4af4756b37a1db28
HK-SHAO <hk-shao@foxmail.com>
2022-10-20 10:29:50 +0800

    update

--- assets/scenes/Main.ts
+++ assets/scenes/Main.ts
@@ -1,11 +1,31 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Label, PageView } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Main')
 export class Main extends Component {
 
+    @property(Label)
+    timeLabel: Label = null;
+
+    @property(PageView)
+    pageView: PageView = null;
+
     start() {
 
+        this.updateTimeLable();
+        this.schedule(this.updateTimeLable, 1000);
+
+    }
+
+    updateTimeLable() {
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        this.timeLabel.string = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
+    }
+
+    onStartButtonClick() {
+        this.pageView.scrollToPage(1, 1);
     }
 
     update(deltaTime: number) {
